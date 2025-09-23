ecb7753675f3469122e0024e36236abdc26c021d
ankitprakash-7 <prakashankit0001@gmail.com>
2025-09-23 19:23:11 +0530

    Add files via upload

---
+++ app-test (1)/src/App.jsx
@@ -0,0 +1,12 @@
+import TextSummarizer from './components/TextSummarizer';
+    import './App.css';
+
+    function App() {
+      return (
+        <main>
+          <TextSummarizer />
+        </main>
+      );
+    }
+
+    export default App;
